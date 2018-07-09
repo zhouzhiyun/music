0f9005fc835480a2ec4e9debdcc1bbc332b26f89
zhouzhiyun <zhouzhiyun0@163.com>
2018-07-09 09:37:12 +0800

    master

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="rank">
+		rank
+	</div>
+</template>
+<script></script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	@import '~common/stylus/variable'
+	@import '~common/stylus/mixin'
+	.rank
+		color:$color-text
+</style>
+
---
+++ src/components/search/search.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="search">
+		search
+	</div>
+</template>
+<script></script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	@import '~common/stylus/variable'
+	@import '~common/stylus/mixin'
+	.search
+		color:$color-text
+</style>
